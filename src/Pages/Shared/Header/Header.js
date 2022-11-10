03568b41dac7dbd5f6aefae9f9cee5c3b5a15e8e
ayeasin14 <ayeasin14@gmail.com>
2022-11-10 20:58:24 +0600

    button toggle added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -11,8 +11,20 @@ const Header = () => {
         <li><Link to="/home" >Home</Link></li>
         <li><Link to="/services" >Services</Link></li>
         <li><Link to="/blogs" >Blogs</Link></li>
-        <li><Link to="/reviews" >My Review</Link></li>
-        <li><Link to="/addservice" >Add Service</Link></li>
+        {
+            user?.uid ?
+
+                <>
+                    <li><Link to="/reviews" >My Review</Link></li>
+                    <li><Link to="/addservice" >Add Service</Link></li>
+                </>
+                :
+
+                <>
+                    <li><Link to="/login" >Login</Link></li>
+                    <li><Link to="/register">Register</Link></li>
+                </>
+        }
 
     </>
 
@@ -24,7 +36,7 @@ const Header = () => {
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                    <ul tabIndex={0} className="menu menu-compact font-semibold dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
                         {menuItems}
                     </ul>
                 </div>
@@ -34,7 +46,7 @@ const Header = () => {
                 </Link>
             </div>
             <div className="navbar-center hidden lg:flex">
-                <ul className="menu menu-horizontal p-0">
+                <ul className="menu menu-horizontal p-0 font-semibold">
                     {menuItems}
                 </ul>
             </div>
@@ -47,10 +59,11 @@ const Header = () => {
                             <button onClick={userSignOut} className='btn btn-outline btn-error ml-2' >Sign Out</button>
                         </div>
                         :
-                        <div className=''>
-                            <Link to="/login" ><button className='btn btn-outline btn-primary mr-2'>Login</button></Link>
-                            <Link to="/register" ><button className='btn btn-outline btn-primary'>Register</button></Link>
-                        </div>
+                        ""
+                        // <div className=''>
+                        //     <Link to="/login" ><button className='btn btn-outline btn-primary mr-2'>Login</button></Link>
+                        //     <Link to="/register" ><button className='btn btn-outline btn-primary'>Register</button></Link>
+                        // </div>
                     }
                 </div>
             </div>
