fdc7236b72aa27c01085bcd983ccc7472f220a01
ayeasin14 <ayeasin14@gmail.com>
2022-11-11 00:07:36 +0600

    some changes

--- src/Pages/Home/ServiceSummery/ServiceSummery.js
+++ src/Pages/Home/ServiceSummery/ServiceSummery.js
@@ -6,7 +6,7 @@ const ServiceSummery = () => {
     const [services, setServices] = useState([])
 
     useEffect(() => {
-        fetch('http://localhost:5000/services')
+        fetch('http://localhost:5000/servicesSummery')
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
